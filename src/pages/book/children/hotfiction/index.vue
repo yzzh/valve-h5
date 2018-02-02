1f8d948d9ec101dc97a066dd419bfde6fd0e7070
yzzh <yzlexo@163.com>
2018-02-02 22:30:23 +0800

    新增增/book/hotfiction和/book/hotnonfiction页面

---
+++ src/pages/book/children/hotfiction/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <bookChild :url="url"></bookChild>
+    <!--<movie-child :url="url"></movie-child>-->
+</template>
+
+<script>
+    import bookChild from '../../../../components/book-child/index.vue';
+    import movieChild from '../../../../components/movie-child/index.vue';
+
+    export default {
+        name: 'hotfiction',
+        data() {
+            return {
+                url: 'https://m.douban.com/rexxar/api/v2/subject_collection/book_fiction/items'
+            }
+        },
+        components: {
+            bookChild,
+            movieChild
+        }
+    }
+</script>
---
+++ src/pages/book/children/hotnonfiction/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <bookChild url="https://m.douban.com/rexxar/api/v2/subject_collection/book_nonfiction/items"></bookChild>
+</template>
+
+<script>
+    import bookChild from '../../../../components/book-child/index.vue';
+
+    export default {
+        name: 'hotnonfiction',
+        components: {
+            bookChild,
+        }
+    }
+</script>
